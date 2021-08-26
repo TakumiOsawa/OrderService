aff359f578f8465566cf68add1fff1985aecbb8e
Takumi Osawa <Takumi.Osawa@nttdata.com>
2021-08-26 20:09:11 +0900

    Importing Eventuate Tram Framework Core.

--- src/main/java/com/ftgo/OrderService/config/OrderServiceConfiguration.java
+++ src/main/java/com/ftgo/OrderService/config/OrderServiceConfiguration.java
@@ -10,7 +10,9 @@ import com.ftgo.OrderService.saga.CreateOrderSaga;
 import com.ftgo.OrderService.proxy.KitchenServiceProxy;
 import io.eventuate.tram.events.publisher.DomainEventPublisher;
 import io.eventuate.tram.sagas.orchestration.SagaInstanceFactory;
+import io.eventuate.tram.sagas.spring.common.EventuateTramSagaCommonConfiguration;
 import io.eventuate.tram.sagas.spring.orchestration.SagaOrchestratorConfiguration;
+import io.eventuate.tram.spring.consumer.common.TramConsumerCommonConfiguration;
 import io.eventuate.tram.spring.events.publisher.TramEventsPublisherConfiguration;
 import io.eventuate.tram.spring.messaging.producer.jdbc.TramMessageProducerJdbcConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -24,7 +26,8 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import({TramEventsPublisherConfiguration.class,
         SagaOrchestratorConfiguration.class,
-        TramMessageProducerJdbcConfiguration.class})
+        TramMessageProducerJdbcConfiguration.class,
+        TramConsumerCommonConfiguration.class})
 public class OrderServiceConfiguration {
     @Bean
     public OrderService orderService(SagaInstanceFactory sagaInstanceFactory,
