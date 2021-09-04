72ef2aa4b80f9a6089515dcab799e51aca4dbff6
Takumi Osawa <Takumi.Osawa@nttdata.com>
2021-09-05 05:45:00 +0900

    Adding Column Annotation at Entities.

--- src/main/resources/sql/create_orders_table.sql
+++ src/main/resources/sql/create_orders_table.sql
@@ -13,6 +13,7 @@ CREATE TABLE orders
 
 CREATE TABLE order_line_items
 (
+    order_order_id bigint NOT NULL,
     quantity bigint NOT NULL,
     menu_item_id bigint NOT NULL,
     name varchar(32) NOT NULL,
@@ -26,6 +27,7 @@ COMMENT ON COLUMN orders.version IS 'バージョン';
 COMMENT ON COLUMN orders.consumer_id IS '利用者ID';
 COMMENT ON COLUMN orders.restaurant_id IS 'レストランID';
 COMMENT ON TABLE order_line_items IS 'メニュー';
+COMMENT ON COLUMN order_line_items.order_order_id IS '注文ID';
 COMMENT ON COLUMN order_line_items.quantity IS '数量';
 COMMENT ON COLUMN order_line_items.menu_item_id IS 'メニューID';
 COMMENT ON COLUMN order_line_items.name IS 'メニュー名';
