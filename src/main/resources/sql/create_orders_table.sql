77f85280cf714fdbe893d86f621a08d22f68cd06
Takumi Osawa <Takumi.Osawa@nttdata.com>
2021-09-06 17:26:30 +0900

    Update application.yml and sql to use MySQL.

--- src/main/resources/sql/create_orders_table.sql
+++ src/main/resources/sql/create_orders_table.sql
@@ -9,7 +9,7 @@ CREATE TABLE orders
     consumer_id bigint NOT NULL,
     restaurant_id bigint NOT NULL,
     PRIMARY KEY (order_id)
-) WITHOUT OIDS;
+);
 
 CREATE TABLE order_line_items
 (
@@ -18,17 +18,4 @@ CREATE TABLE order_line_items
     menu_item_id bigint NOT NULL,
     name varchar(32) NOT NULL,
     price bigint NOT NULL
-) WITHOUT OIDS;
-
-COMMENT ON TABLE orders IS '注文';
-COMMENT ON COLUMN orders.order_id IS '注文ID';
-COMMENT ON COLUMN orders.state IS '注文状態';
-COMMENT ON COLUMN orders.version IS 'バージョン';
-COMMENT ON COLUMN orders.consumer_id IS '利用者ID';
-COMMENT ON COLUMN orders.restaurant_id IS 'レストランID';
-COMMENT ON TABLE order_line_items IS 'メニュー';
-COMMENT ON COLUMN order_line_items.order_order_id IS '注文ID';
-COMMENT ON COLUMN order_line_items.quantity IS '数量';
-COMMENT ON COLUMN order_line_items.menu_item_id IS 'メニューID';
-COMMENT ON COLUMN order_line_items.name IS 'メニュー名';
-COMMENT ON COLUMN order_line_items.price IS '単価';
+);
