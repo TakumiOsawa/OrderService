a6169d780a1df3eec153220c0019257df86c5c7f
Takumi Osawa <Takumi.Osawa@nttdata.com>
2021-09-08 17:44:29 +0900

    Adding sql to create eventuate database.

--- src/main/resources/sql/create_eventuate_tables.sql
+++ src/main/resources/sql/create_eventuate_tables.sql
@@ -2,12 +2,12 @@ CREATE DATABASE IF NOT EXISTS eventuate;
 
 USE eventuate;
 
-CREATE TABLE message
+CREATE TABLE eventuate.message
 (
-    id varchar NOT NULL,
-    destination varchar NOT NULL,
-    headers varchar NOT NULL,
-    payload varchar NOT NULL,
+    id varchar(128) NOT NULL,
+    destination varchar(128) NOT NULL,
+    headers varchar(128) NOT NULL,
+    payload varchar(512) NOT NULL,
     creation_time timestamp NOT NULL,
     published int NOT NULL,
     PRIMARY KEY (order_id)
