9568a7ce4b908e2f7e2853b0598fb0407e9464f6
Takumi Osawa <Takumi.Osawa@nttdata.com>
2021-09-08 17:49:31 +0900

    Adding sql to create eventuate database.

--- src/main/resources/sql/create_eventuate_tables.sql
+++ src/main/resources/sql/create_eventuate_tables.sql
@@ -8,7 +8,7 @@ CREATE TABLE eventuate.message
 (
     id varchar(128) NOT NULL,
     destination varchar(128) NOT NULL,
-    headers varchar(128) NOT NULL,
+    headers varchar(512) NOT NULL,
     payload varchar(512) NOT NULL,
     creation_time timestamp NOT NULL,
     published int NOT NULL,
