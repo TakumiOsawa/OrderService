d0f7362cd4b9a87f1fbf0687d55f65b3169c2f61
Takumi Osawa <Takumi.Osawa@nttdata.com>
2021-09-08 18:24:13 +0900

    Adding sql to create eventuate database.

--- src/main/resources/sql/create_eventuate_tables.sql
+++ src/main/resources/sql/create_eventuate_tables.sql
@@ -2,15 +2,63 @@ CREATE DATABASE IF NOT EXISTS eventuate;
 
 USE eventuate;
 
-DROP TABLE IF EXISTS eventuate.message;
+CREATE SCHEMA IF NOT EXISTS eventuate AUTHORIZATION SA;
 
-CREATE TABLE eventuate.message
-(
-    id varchar(64) NOT NULL,
-    destination varchar(64) NOT NULL,
-    headers varchar(512) NOT NULL,
-    payload varchar(512) NOT NULL,
-    creation_time datetime NOT NULL,
-    published int NOT NULL,
-    PRIMARY KEY (id)
+drop table if exists eventuate.message;
+
+CREATE TABLE eventuate.message (
+    ID VARCHAR(1000) PRIMARY KEY,
+    DESTINATION VARCHAR(1000) NOT NULL,
+    HEADERS VARCHAR(1000) NOT NULL,
+    PAYLOAD VARCHAR(1000) NOT NULL,
+    CREATION_TIME BIGINT
+);
+
+drop table if exists eventuate.received_messages;
+
+CREATE TABLE eventuate.received_messages (
+    CONSUMER_ID VARCHAR(1000),
+    MESSAGE_ID VARCHAR(1000),
+    CREATION_TIME BIGINT,
+    PRIMARY KEY(CONSUMER_ID, MESSAGE_ID)
+);
+
+DROP Table IF Exists saga_instance_participants;
+DROP Table IF Exists saga_instance;
+DROP Table IF Exists saga_lock_table;
+DROP Table IF Exists saga_stash_table;
+
+CREATE TABLE saga_instance_participants (
+    saga_type VARCHAR(255) NOT NULL,
+    saga_id VARCHAR(100) NOT NULL,
+    destination VARCHAR(100) NOT NULL,
+    resource VARCHAR(100) NOT NULL,
+    PRIMARY KEY(saga_type, saga_id, destination, resource)
+);
+
+CREATE TABLE saga_instance(
+    saga_type VARCHAR(255) NOT NULL,
+    saga_id VARCHAR(100) NOT NULL,
+    state_name VARCHAR(100) NOT NULL,
+    last_request_id VARCHAR(100),
+    end_state INT(1),
+    compensating INT(1),
+    saga_data_type VARCHAR(1000) NOT NULL,
+    saga_data_json VARCHAR(1000) NOT NULL,
+    PRIMARY KEY(saga_type, saga_id)
+);
+
+create table saga_lock_table(
+    target VARCHAR(100) PRIMARY KEY,
+    saga_type VARCHAR(255) NOT NULL,
+    saga_Id VARCHAR(100) NOT NULL
+);
+
+create table saga_stash_table(
+    message_id VARCHAR(100) PRIMARY KEY,
+    target VARCHAR(100) NOT NULL,
+    saga_type VARCHAR(255) NOT NULL,
+    saga_id VARCHAR(100) NOT NULL,
+    message_headers VARCHAR(1000) NOT NULL,
+    message_payload VARCHAR(1000) NOT NULL
 );
