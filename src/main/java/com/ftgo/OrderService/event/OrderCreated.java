7641be990b68d0a36d11fe2bc8a60e7f3192ec98
Takumi Osawa <Takumi.Osawa@nttdata.com>
2021-09-13 07:08:08 +0900

    Trying API Composition.

--- src/main/java/com/ftgo/OrderService/event/OrderCreated.java
+++ src/main/java/com/ftgo/OrderService/event/OrderCreated.java
@@ -1,15 +1,35 @@
 package com.ftgo.OrderService.event;
 
 import com.ftgo.OrderService.domain.order.OrderDetails;
+import lombok.Data;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
 
 /**
  * Event to signify order created.
  */
 
+@Data
 public class OrderCreated implements OrderDomainEvent {
     private final OrderDetails orderDetails;
 
     public OrderCreated(OrderDetails orderDetails) {
         this.orderDetails = orderDetails;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
     }
 }
